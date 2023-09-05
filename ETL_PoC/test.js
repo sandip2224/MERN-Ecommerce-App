96f2d54a0b6dfc267107cbc3f7e87430e289a7a0
Sandipan Das <61842142+sandip2224@users.noreply.github.com>
2023-09-05 17:11:33 +0530

    Update test.js

--- ETL_PoC/test.js
+++ ETL_PoC/test.js
@@ -1,39 +1,31 @@
 const MAX_RETRY_COUNT = 3;
 const RETRY_DELAY_MS = 1000; // Adjust this delay as needed (in milliseconds)
 
-const backupAdvanceDetail = async (requestQueue, updateTable, signal) => {
-  for (let i = 0; i < requestQueue?.length; i += 1) {
-    await makeRequestWithRetry(requestQueue?.[i], updateTable, signal);
-  }
-};
-
-const makeRequestWithRetry = async (requestData, updateTable, signal, retryCount = 0) => {
-  try {
+const backupAdvanceDetail = (requestQueue, updateTable, signal) => {
+  const makeRequest = (requestData, retryCount = 0) => {
     const portfolioReqList = requestData.portfolioReqList;
-    const response = await APIService(
-      { portfolioReqList },
-      apiConfig.portfolioAdvanceDetailEndpoint,
-      undefined,
-      true,
-      false,
-      signal
-    );
-
-    updateTable(requestData, response);
-  } catch (error) {
-    if (error.response && error.response.status >= 500 && retryCount < MAX_RETRY_COUNT) {
-      // Retry after a delay
-      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY_MS));
-      await makeRequestWithRetry(requestData, updateTable, signal, retryCount + 1);
-    } else {
-      // Handle the error after max retries or for non-5xx errors
-      const errorValue = {
-        response: {
-          measureValue: "ERROR",
-          benchMarkname: null
+    APIService({ portfolioReqList }, apiConfig.portfolioAdvanceDetailEndpoint, undefined, true, false, signal)
+      .then(response => {
+        updateTable(requestData, response);
+      })
+      .catch(error => {
+        if (retryCount < MAX_RETRY_COUNT) {
+          // Retry after a delay
+          setTimeout(() => makeRequest(requestData, retryCount + 1), RETRY_DELAY_MS);
+        } else {
+          // Handle the error after max retries
+          const errorValue = {
+            response: {
+              measureValue: "ERROR",
+              benchMarkname: null
+            }
+          };
+          updateTable(requestData, errorValue);
         }
-      };
-      updateTable(requestData, errorValue);
-    }
+      });
+  };
+
+  for (let i = 0; i < requestQueue?.length; i += 1) {
+    makeRequest(requestQueue?.[i]);
   }
 };
