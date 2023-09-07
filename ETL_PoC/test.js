a80f9fbc54b2e7a4f8221a0f1900e6323b20cfb4
Sandipan Das <61842142+sandip2224@users.noreply.github.com>
2023-09-07 13:38:26 +0530

    Update test.js

--- ETL_PoC/test.js
+++ ETL_PoC/test.js
@@ -1,20 +1,22 @@
-const MAX_RETRY_COUNT = 3;
-const RETRY_DELAY_MS = 1000; // Adjust this delay as needed (in milliseconds)
-
-const logAndRetry = (i, updateTable, signal, retryCount) => {
+const logAndRetry = (i, updateTable, signal, retryCount, requestQueue) => {
   console.log(`Iteration ${i}: Request failed`);
   console.log(`Retrying in ${RETRY_DELAY_MS} milliseconds... Retry count: ${retryCount + 1}`);
   
   // Retry after a delay
-  setTimeout(() => makeRequest(i, updateTable, signal, retryCount + 1), RETRY_DELAY_MS);
+  setTimeout(() => makeRequest(i, updateTable, signal, retryCount + 1, requestQueue), RETRY_DELAY_MS);
 };
 
-const makeRequest = (i, updateTable, signal, retryCount = 0) => {
+const makeRequest = (i, updateTable, signal, retryCount, requestQueue) => {
+  if (i >= requestQueue?.length) {
+    console.log(`No more requests. Iteration ${i} reached the end of the queue.`);
+    return;
+  }
+
   const requestData = requestQueue?.[i];
   if (!requestData) {
     console.log(`Iteration ${i}: Request data is null or undefined. Skipping this request.`);
     // Proceed to the next iteration
-    processNextRequest(i + 1, updateTable, signal);
+    processNextRequest(i + 1, updateTable, signal, requestQueue);
     return;
   }
 
@@ -24,11 +26,11 @@ const makeRequest = (i, updateTable, signal, retryCount = 0) => {
       console.log(`Iteration ${i}: Request succeeded`);
       updateTable(requestData, response);
       // Proceed to the next iteration on success
-      processNextRequest(i + 1, updateTable, signal);
+      processNextRequest(i + 1, updateTable, signal, requestQueue);
     })
     .catch(error => {
       if (retryCount < MAX_RETRY_COUNT) {
-        logAndRetry(i, updateTable, signal, retryCount);
+        logAndRetry(i, updateTable, signal, retryCount, requestQueue);
       } else {
         // Handle the error after max retries
         console.log(`Iteration ${i}: Max retry count reached`);
@@ -40,17 +42,17 @@ const makeRequest = (i, updateTable, signal, retryCount = 0) => {
         };
         updateTable(requestData, errorValue);
         // Proceed to the next iteration after max retries
-        processNextRequest(i + 1, updateTable, signal);
+        processNextRequest(i + 1, updateTable, signal, requestQueue);
       }
     });
 };
 
-const processNextRequest = (i, updateTable, signal) => {
+const processNextRequest = (i, updateTable, signal, requestQueue) => {
   if (i < requestQueue?.length) {
-    makeRequest(i, updateTable, signal);
+    makeRequest(i, updateTable, signal, 0, requestQueue);
   }
 };
 
 const backupAdvanceDetail = (requestQueue, updateTable, signal) => {
-  processNextRequest(0, updateTable, signal);
+  processNextRequest(0, updateTable, signal, requestQueue);
 };
